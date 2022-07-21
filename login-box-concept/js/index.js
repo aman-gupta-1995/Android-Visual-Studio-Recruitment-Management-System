6d7ebce84f796084e294547649661de2ef2dc22e
AMAN GUPTA <amangupta1995.ag@gmail.com>
2022-07-22 03:17:30 +0530

    Update index.js

--- login-box-concept/js/index.js
+++ login-box-concept/js/index.js
@@ -6,7 +6,7 @@
 
 // function myFunction() {
 //   if (window.pageYOffset >= sticky) {  
-//     navbar.classList.add("sticky")
+//     navbar.classList.add("sticky") 
 //   } else {  
 //     navbar.classList.remove("sticky");
 //   }
